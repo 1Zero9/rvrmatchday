8a577659cad6ce5982fadd67d003c0ab3eec2a14
1Zero9 <scranfield@gmail.com>
2025-08-11 18:13:45 +0100

    chore: Tailwind v4 via @tailwindcss/postcss; baseline app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const el = document.getElementById('root');
+
+if (!el) {
+  document.body.innerHTML = '<pre style="padding:1rem;font-family:monospace;color:#333">Error: Missing <div id="root"></div> in index.html</pre>';
+} else {
+  createRoot(el).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
