a1f61d8e058df13760c9b4706f54b499d0401fee
1Zero9 <scranfield@gmail.com>
2025-08-18 12:45:43 +0000

    Add Layout component to wrap pages

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-6">{children}</main>
+      <Footer />
+    </div>
+  );
+}
